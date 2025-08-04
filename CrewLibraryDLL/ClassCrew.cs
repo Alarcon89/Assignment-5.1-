b12d44ec02bdc17d7a647ef7c932cae7d375ae17
Alarcon89 <158241523+Alarcon89@users.noreply.github.com>
2025-08-03 20:10:08 -0500

    Add files via upload

---
+++ CrewLibraryDLL/ClassCrew.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    
+    public class ClassCrew
+    {
+        //create properties for this class
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MissionName { get; set; }
+        
+
+        // Here I am creating a contructor for the ClassCrew class, 
+        // to populate the values of an object when the object is created.
+
+        public ClassCrew(string firstName, string lastName, string missionName)
+        {
+            
+            // Here I am setting a property with data that has been passed
+            // set the firstname, last name and mission name
+            FirstName = firstName;
+            LastName = lastName;
+            MissionName = missionName;
+
+        }
+
+       
+
+        //create a method so the person can do stuff 
+        // this method will return a string with the first and last name of the crew member
+        // in the list. 
+        public string GetFullName()
+        {
+            // Here I am creating a string to hold data to return from this method. 
+            string result = string.Empty;
+            // Build data string to return
+            result = FirstName + " ready to fight " + "lets go! ";
+            // Now this line right here will return the data I built and stuffed into the string variable. 
+            return result;
+        }
+    }
+}
---
+++ CrewLibraryDLL/ClassMission.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    // In this case, classmission is the child class of ClassCrew
+    public class ClassMission : ClassCrew
+    {
+        // give our crew their own properties
+        public string PlanetName { get; set; }
+
+
+
+        // Create a constructor that will pass data to the parent class
+        // And require the mission name that will not be passed up to the parent class
+        public ClassMission(string planetName, string missionName, string firstName, string lastName) 
+            : base(firstName, lastName, planetName)
+        {
+            // The only property that will not be passed up to the parent class is MissionName because
+            // the other we are passing up our parent class for it to handle. 
+
+            PlanetName = planetName;
+        }
+
+        public ClassMission(string firstName, string lastName, string missionName) : base(firstName, lastName, missionName)
+        {
+        }
+
+        // Creating a method so the mission can do stuff
+        // This method will return a string with the mission name and the first and last name of the crew member
+        public string GetMissionDetails()
+        {
+            string result;
+            result = FirstName + " in a mission at Mars " + PlanetName + " for Democracy!";
+            return result;
+        }
+
+
+
+
+    }
+}
